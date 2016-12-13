c58cb31217503cbb0270737ba574dd5e5866ce7d
jo <jo.van.montfort1@telenet.be>
2016-12-13 15:33:35 +0100

    JPA02

--- src/main/java/be/vdab/repositories/RepositoriesConfig.java
+++ src/main/java/be/vdab/repositories/RepositoriesConfig.java
@@ -2,29 +2,38 @@ package be.vdab.repositories;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import be.vdab.entities.Filiaal;
+import be.vdab.valueobjects.Adres;
 
 @Configuration
 @ComponentScan
 public class RepositoriesConfig {
+	@Autowired
+	private DataSource dataSource;
 
 	@Bean
-	DataSourceTransactionManager transactionManager(DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource);
+	JpaTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+		return new JpaTransactionManager(entityManagerFactory.getObject());
 	}
 
 	@Bean
-	JdbcTemplate jdbcTemplate(DataSource dataSource) {
-		return new JdbcTemplate(dataSource);
-	}
-
-	@Bean
-	NamedParameterJdbcTemplate namedJdbcTemplate(DataSource dataSource) {
-		return new NamedParameterJdbcTemplate(dataSource);
+	LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setDataSource(dataSource);
+		factory.setPackagesToScan(Filiaal.class.getPackage().getName(), Adres.class.getPackage().getName());
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setShowSql(true);
+		factory.setJpaVendorAdapter(adapter);
+		factory.getJpaPropertyMap().put("hibernate.format_sql", true);
+		factory.getJpaPropertyMap().put("hibernate.use_sql_comments", true);
+		return factory;
 	}
 }
